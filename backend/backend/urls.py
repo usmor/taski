88b492cee45ecc40384e7de9d0c61a06fe44f563
usmor <7887330@gmail.com>
2026-08-19 00:30:14 +0300

    fix code style

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,12 @@
+from api import views
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from api import views
-
 router = routers.DefaultRouter()
-router.register('tasks', views.TaskView, 'task')
+router.register("tasks", views.TaskView, "task")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
